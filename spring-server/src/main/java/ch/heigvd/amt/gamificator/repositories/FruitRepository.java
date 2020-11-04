a8af6831f879a29f335f89ef0c8da24eed6ea5ef
Didier Page <didier.page@heig-vd.ch>
2020-11-04 13:27:55 +0100

    Rename packages name

---
+++ spring-server/src/main/java/ch/heigvd/amt/gamificator/repositories/FruitRepository.java
@@ -0,0 +1,11 @@
+package ch.heigvd.amt.gamificator.repositories;
+
+import ch.heigvd.amt.gamificator.entities.FruitEntity;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Olivier Liechti on 26/07/17.
+ */
+public interface FruitRepository extends CrudRepository<FruitEntity, Long>{
+
+}
