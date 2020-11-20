84233c98e0ba03f43e60f838ab42ba8d9fbb68f1
Lougranou <bruno.legrand@heig-vd.ch>
2020-11-20 16:25:38 +0100

    update mysql init script

--- docker/database/init.sql
+++ docker/database/init.sql
@@ -1,1 +1,5 @@
+create database if not exists `gamificator_db`;
+
+USE `gamificator_db`;
+
 grant all on gamificator_db.* to 'springuser'@'%'; -- Gives all privileges to the new user on the newly created database
