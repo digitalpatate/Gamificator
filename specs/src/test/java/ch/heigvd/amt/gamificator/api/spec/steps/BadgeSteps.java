7330f8eb68a9f57714f3049078d364b26f75d759
Lougranou <bruno.legrand@heig-vd.ch>
2020-12-02 17:55:30 +0100

    [WIP] add badge.feature and a scenario skeleton

--- specs/src/test/java/ch/heigvd/amt/gamificator/api/spec/steps/BadgeSteps.java
+++ specs/src/test/java/ch/heigvd/amt/gamificator/api/spec/steps/BadgeSteps.java
@@ -6,6 +6,7 @@ import ch.heigvd.amt.gamificator.api.dto.BadgeCreateCommand;
 import ch.heigvd.amt.gamificator.api.dto.BadgeDTO;
 import ch.heigvd.amt.gamificator.api.dto.PointScaleCreateCommand;
 import ch.heigvd.amt.gamificator.api.spec.helpers.Environment;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
 public class BadgeSteps extends Steps {
@@ -15,5 +16,25 @@ public class BadgeSteps extends Steps {
     public BadgeSteps(Environment environment) {
         super(environment);
     }
+
+    @Given("I POST the badge payload to the /badges endpoints")
+    public void iPOSTTheBadgePayloadToTheBadgesEndpoints() throws Throwable {
+        try {
+            ApiResponse apiResponse =
+                    getApi().createBadgeWithHttpInfo(badgeCreateCommand.getName(), badgeCreateCommand.getDescription());
+            getEnvironment().processApiResponse(apiResponse);
+        } catch (ApiException e) {
+            getEnvironment().processApiException(e);
+        }
+    }
+
+    @Given("there is a badge payload")
+    public void thereIsABadgePayload() {
+        badgeCreateCommand = new BadgeCreateCommand();
+        badgeCreateCommand.setName("SuperMan");
+        badgeCreateCommand.setDescription("Badge for super hero only, includes : " +
+                "Super Strength, Super Speed, Enhanced Leaping, Super Durability and Super Senses");
+    }
+
 
 }
---
+++ specs/src/test/resources/scenarios/badge.feature
@@ -0,0 +1,7 @@
+Feature: Basic operations on badges
+
+  Background:
+    Given there is a Gamificator server
+
+  Scenario: get a point scale by id
+    Given there is a badge payload
