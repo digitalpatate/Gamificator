30be4df4f18dc719d0440cd7d4543a8732d03c57
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2020-12-09 09:58:56 +0100

    added a mapper to event

---
+++ impl/src/main/java/ch/heigvd/amt/gamificator/api/event/EventMapper.java
@@ -0,0 +1,14 @@
+package ch.heigvd.amt.gamificator.api.event;
+
+import ch.heigvd.amt.gamificator.api.model.CreateEventCommand;
+import ch.heigvd.amt.gamificator.entities.Event;
+
+public class EventMapper {
+    public static Event toEntity(CreateEventCommand createEventCommand) {
+        Event event = new Event();
+        //event.setTimestamp(createEventCommand.getTimestamp());
+        event.setType(createEventCommand.getType());
+
+        return event;
+    }
+}
