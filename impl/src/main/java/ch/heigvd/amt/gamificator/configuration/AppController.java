bcb08b31faa327e8edd53dee62fac70af865c99f
Didier Page <didier.page@heig-vd.ch>
2020-12-11 13:37:13 +0100

    Add a new endpoint for the test (#35)

--- impl/src/main/java/ch/heigvd/amt/gamificator/configuration/AppController.java
+++ impl/src/main/java/ch/heigvd/amt/gamificator/configuration/AppController.java
@@ -1,5 +1,7 @@
 package ch.heigvd.amt.gamificator.configuration;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,10 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Home redirection to swagger api documentation 
  */
 @Controller
-public class HomeController {
+public class AppController {
  /*   @RequestMapping(value = "/")
     public String index() {
         System.out.println("swagger-ui.html");
         return "redirect:swagger-ui/";
     }*/
+
+    @RequestMapping("/healthcheck")
+    public ResponseEntity healthcheck() {
+        return  new ResponseEntity(HttpStatus.OK);
+    }
 }
