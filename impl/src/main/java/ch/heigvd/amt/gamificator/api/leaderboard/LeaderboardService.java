4b0585c929d9abee79a74e1c22f05b0ecf6a9a04
Didier Page <didier.page@heig-vd.ch>
2021-01-21 12:10:19 +0100

    Return 404 if pointScale cannot be found

--- impl/src/main/java/ch/heigvd/amt/gamificator/api/leaderboard/LeaderboardService.java
+++ impl/src/main/java/ch/heigvd/amt/gamificator/api/leaderboard/LeaderboardService.java
@@ -6,8 +6,11 @@ import ch.heigvd.amt.gamificator.api.model.UserScoreDTO;
 import ch.heigvd.amt.gamificator.api.user.PagedUserDTO;
 import ch.heigvd.amt.gamificator.api.user.UserMapper;
 import ch.heigvd.amt.gamificator.api.user.UserService;
+import ch.heigvd.amt.gamificator.entities.PointScale;
 import ch.heigvd.amt.gamificator.entities.PointsReward;
 import ch.heigvd.amt.gamificator.entities.Reputation;
+import ch.heigvd.amt.gamificator.exceptions.NotFoundException;
+import ch.heigvd.amt.gamificator.repositories.PointScaleRepository;
 import ch.heigvd.amt.gamificator.repositories.ReputationRepository;
 import ch.heigvd.amt.gamificator.services.SecurityContextService;
 import lombok.AllArgsConstructor;
@@ -24,6 +27,8 @@ import java.util.List;
 public class LeaderboardService {
 
     private final ReputationRepository reputationRepository;
+
+    private final PointScaleRepository pointScaleRepository;
 
     @Autowired
     private UserService userService;
@@ -62,9 +67,14 @@ public class LeaderboardService {
         return leaderBoardDTO;
     }
 
-    public LeaderBoardDTO getLeaderboardOnPointScale(String pointScaleName, Pageable pageable) {
+    public LeaderBoardDTO getLeaderboardOnPointScale(String pointScaleName, Pageable pageable) throws NotFoundException {
         long applicationId = securityContextService.getApplicationIdFromAuthentifiedApp();
 
+        Optional<PointScale> oPointScale = pointScaleRepository.findByNameAndApplicationId(pointScaleName,applicationId);
+
+        if(oPointScale.isEmpty()){
+            throw new NotFoundException(String.format("The pointScale by name %s cannot be found",pointScaleName));
+        }
         PagedUserDTO userDTOs = userService.findAllByApplicationPageable(applicationId,pageable);
 
         List<UserScoreDTO> userScoreDTOs = new LinkedList<>();
